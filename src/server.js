959362c423d542501123223a1a29a9e1d98280e1
Justino Soares <justinocsoares123@gmail.com>
2025-12-22 06:55:18 +0100

    deploy

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const routerMain = require("./routes/router.js");
-const master = require("./middleware/master.middleware.js")();
+//const master = require("./middleware/master.middleware.js")();
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
